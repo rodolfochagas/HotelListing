952cc7171765bffd4c06624a447f1102ba87d216
rodolfochagas <rodolfochagas@id.uff.br>
2021-07-03 20:43:04 -0300

    Adding List of Hotels to Country database entity

--- HotelListing/Data/Country.cs
+++ HotelListing/Data/Country.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace HotelListing.Data
 {
@@ -10,5 +7,8 @@ namespace HotelListing.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
+
+        //Property that facilitates queries, but is not on the database (no need for migration)
+        public virtual IList<Hotel> Hotels { get; set; }
     }
 }
